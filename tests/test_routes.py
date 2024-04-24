bed233a3c0fbdf122a5dcfaec7b92a100176438b
Aditya Karnam <akarnam37@gmail.com>
2024-04-24 18:58:44 -0500

    fix: add tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,6 +6,11 @@ from ff_routing_fastapi.main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_feature():
     response = client.get("/feature_flag/feature")
+    assert response.json() == {'message': 'hello'}
+
+
+def test_feature_flag():
+    response = client.get("/feature_flag/feature_async")
     assert response.json() == {'message': 'hello'}
